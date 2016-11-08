b6b69297f9acc4a621d7fb25d4cd7dba97b8bee0
EveGayer <evelin.geier@googlemail.com>
2016-11-09 00:47:25 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/nyan/nyancat/WorkActivity.java
@@ -0,0 +1,17 @@
+package com.example.nyan.nyancat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andreas on 04.11.16.
+ */
+public class WorkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
